9d86552367a6d9caafe18dc5e760cb49a463db86
omar.gomez <omar.gomez@bnamericas.com>
2019-05-31 17:42:37 -0400

    add table and dummy data

--- src/app/modules/informes/existence/existence.component.ts
+++ src/app/modules/informes/existence/existence.component.ts
@@ -7,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExistenceComponent implements OnInit {
  
-  data = [{selected: false, nombre: "Lapiz BIC azul", codigo:"1546848988",}];
+  data = [
+    {selected: false, nombre: "Lapiz BIC azul", codigo:"1546848988", cantidad: 12, unitario: 2500},
+    {selected: true, nombre: "Lapiz BIC rojo", codigo:"1546848978", cantidad: 154, unitario: 500}
+  ];
+  total: number;
   constructor() { }
 
   ngOnInit() {
+    this.total = this.data.reduce((sum, item) => sum + (item.cantidad + item.unitario), 0)
+  }
+
+  selectAll(e) {
+    this.data.forEach(item => {
+      item.selected = e.target.checked;
+    });
   }
 
 }

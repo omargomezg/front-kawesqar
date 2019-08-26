621c7177b3153863c847ba57efe92f452be6359a
Omar Gómez <omar.fdo.gomez@gmail.com>
2019-08-25 22:55:06 -0400

    add some files

---
+++ src/app/core/models/database/OutputFlowType.model.ts
@@ -0,0 +1,5 @@
+export class OutputFlowTypeModel {
+    public id: number;
+    public name: string;
+    public code: string;
+}
---
+++ src/app/core/services/output-flow-type.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ShoppingCartModel} from 'src/app/core/models/database/ShoppingCart.model';
+import {environment} from 'src/environments/environment';
+import {map} from 'rxjs/operators';
+import {OutputFlowTypeModel} from 'src/app/core/models/database/OutputFlowType.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OutputFlowTypeService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) {
+  }
+  getAvailableOutputFlows(rut: string): Observable<OutputFlowTypeModel[]> {
+    return this.http
+      .get<OutputFlowTypeModel[]>(`${environment.apiUrl}/api/egress/${rut}`)
+      .pipe(map(data => data));
+  }
+}

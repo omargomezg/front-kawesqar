0327ad529ed0d45472753dc4b0756e2d5ce670aa
Omar Gómez <omar.gomez@bnamericas.com>
2019-10-26 01:42:58 -0300

    update creation user

--- src/app/modules/usuario/usuario-routing.module.ts
+++ src/app/modules/usuario/usuario-routing.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormComponent } from './form/form.component';
-import { ListUsersComponent } from './list-users/list-users.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormComponent} from './form/form.component';
+import {ListUsersComponent} from './list-users/list-users.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: FormComponent },
-    { path: 'edit/:rut', component: FormComponent },
-    { path: 'list', component: ListUsersComponent }
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: FormComponent},
+  {path: 'edit/:rut', component: FormComponent},
+  {path: 'new', component: FormComponent},
+  {path: 'list', component: ListUsersComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 
 export class UsuarioRoutingModules {
